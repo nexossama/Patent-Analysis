f8e9bc8368dd127332084c1f7a03d0e2b4083b77
Hamza MOTASSIM <motassimhamza99@gmail.com>
2024-03-13 00:28:53 +0000

    static

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/home')
 def index():
-    return 'indexx'
+    return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

0be5fa2d1a46af841c3a20f3e1d2f00dbbfc18f7
AymaneMG <aymanemaghouti16@gmail.com>
2024-03-16 22:57:58 +0000

    extract urls and search equation

--- Sourcing-Google Patent-/sourcing/csv_files/push_Data_hdfs.py
+++ Sourcing-Google Patent-/sourcing/csv_files/push_Data_hdfs.py
@@ -11,6 +11,6 @@ def push_data(input_file):
 
     print(f"File {input_file} uploaded to HDFS")
 
-csv_files = ['downloaded_file1.csv','downloaded_file0.csv']
+csv_files = ['downloaded_file0.csv','downloaded_file1.csv','downloaded_file2.csv','downloaded_file3.csv','downloaded_file4.csv','downloaded_file5.csv','downloaded_file6.csv','downloaded_file7.csv']
 for file in csv_files :
     push_data(file)
